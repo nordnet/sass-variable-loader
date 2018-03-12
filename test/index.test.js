fc031683eecfa03c17275642d96a73c0adaefe5e
Oleg Maslov <maslovbox@gmail.com>
2018-03-12 17:40:32 +0300

    ESLint and Prettier setup

--- test/index.test.js
+++ test/index.test.js
@@ -1,99 +1,101 @@
+/* eslint-disable import/no-extraneous-dependencies, max-len */
 const { expect } = require('chai');
 const getVariables = require('../src/get-variables');
 const parseVariables = require('../src/parse-variables');
 
-context('without comments', function() {
-    const sass = '$gray-base: #000 !default;\n$gray-darker: lighten($gray-base, 13.5%) !default; // #222\n$gray-dark: lighten($gray-base, 20%) !default;  // #333\n$gray:  lighten($gray-base, 33.5%) !default; // #555\n$gray-light:  lighten($gray-base, 46.7%) !default; // #777\n$gray-lighter:  lighten($gray-base, 93.5%) !default; // #eee';
-    const variables = getVariables(sass);
+context('without comments', () => {
+  const sass =
+    '$gray-base: #000 !default;\n$gray-darker: lighten($gray-base, 13.5%) !default; // #222\n$gray-dark: lighten($gray-base, 20%) !default;  // #333\n$gray:  lighten($gray-base, 33.5%) !default; // #555\n$gray-light:  lighten($gray-base, 46.7%) !default; // #777\n$gray-lighter:  lighten($gray-base, 93.5%) !default; // #eee';
+  const variables = getVariables(sass);
 
-    describe('getVariables()', function() {
-      it('should return an array with 6 items', function() {
-        expect(variables).to.be.a('array');
-        expect(variables).to.have.length(6);
-      });
+  describe('getVariables()', () => {
+    it('should return an array with 6 items', () => {
+      expect(variables).to.be.a('array');
+      expect(variables).to.have.length(6);
     });
+  });
 
-    describe('parseVariables()', function() {
-      it('should return an object with the key grayBase', function() {
-        const result = parseVariables(variables);
-        expect(result).to.be.a('object');
-        expect(result).to.include.keys('grayBase');
-      });
+  describe('parseVariables()', () => {
+    it('should return an object with the key grayBase', () => {
+      const result = parseVariables(variables);
+      expect(result).to.be.a('object');
+      expect(result).to.include.keys('grayBase');
     });
+  });
 
-    describe('parseVariables({ preserveVariableNames: true })', function() {
-      it('should return an object with the key gray-base', function() {
-        const result = parseVariables(variables, { preserveVariableNames: true });
-        expect(result).to.be.a('object');
-        expect(result).to.include.keys('gray-base');
-      });
+  describe('parseVariables({ preserveVariableNames: true })', () => {
+    it('should return an object with the key gray-base', () => {
+      const result = parseVariables(variables, { preserveVariableNames: true });
+      expect(result).to.be.a('object');
+      expect(result).to.include.keys('gray-base');
     });
-})
+  });
+});
 
-context('with comments', function() {
-    const sass = `$one: 123;
+context('with comments', () => {
+  const sass = `$one: 123;
 $x: $one;
-// $y: $two; // ERROR - $two not existed, but it's commented`
-    const variables = getVariables(sass);
+// $y: $two; // ERROR - $two not existed, but it's commented`;
+  const variables = getVariables(sass);
 
-    describe('getVariables()', function() {
-      it('should return an array with 2 items', function() {
-        expect(variables).to.be.a('array');
-        expect(variables).to.have.length(2);
-      });
+  describe('getVariables()', () => {
+    it('should return an array with 2 items', () => {
+      expect(variables).to.be.a('array');
+      expect(variables).to.have.length(2);
     });
+  });
 
-    describe('parseVariables()', function() {
-      it('should return an object with the key one', function() {
-        const result = parseVariables(variables);
-        expect(result).to.be.a('object');
-        expect(result).to.include.keys('one');
-      });
-      it('should not return an object with the key y', function() {
-        const result = parseVariables(variables);
-        expect(result).to.be.a('object');
-        expect(result).to.not.include.keys('y');
-      });
+  describe('parseVariables()', () => {
+    it('should return an object with the key one', () => {
+      const result = parseVariables(variables);
+      expect(result).to.be.a('object');
+      expect(result).to.include.keys('one');
     });
-})
+    it('should not return an object with the key y', () => {
+      const result = parseVariables(variables);
+      expect(result).to.be.a('object');
+      expect(result).to.not.include.keys('y');
+    });
+  });
+});
 
-context('.sass format', function() {
-    const sass = `$one: 123
+context('.sass format', () => {
+  const sass = `$one: 123
 $x: $one
-`
-    const variables = getVariables(sass);
+`;
+  const variables = getVariables(sass);
 
-    describe('getVariables()', function() {
-      it('should return an array with 2 items', function() {
-        expect(variables).to.be.a('array');
-        expect(variables).to.have.length(2);
-      });
+  describe('getVariables()', () => {
+    it('should return an array with 2 items', () => {
+      expect(variables).to.be.a('array');
+      expect(variables).to.have.length(2);
+    });
+  });
+
+  describe('parseVariables()', () => {
+    it('should return an object with the key one', () => {
+      const result = parseVariables(variables);
+      expect(result).to.be.a('object');
+      expect(result).to.include.keys('one');
+    });
+  });
+});
+
+context('empty sass-file', () => {
+  describe('getVariables()', () => {
+    function testFn() {
+      const sass = '';
+      return parseVariables(getVariables(sass));
+    }
+
+    it('should not throw', () => {
+      expect(testFn).to.not.throw(TypeError);
     });
 
-    describe('parseVariables()', function() {
-      it('should return an object with the key one', function() {
-        const result = parseVariables(variables);
-        expect(result).to.be.a('object');
-        expect(result).to.include.keys('one');
-      });
+    it('should be an empty object', () => {
+      const variables = testFn();
+      expect(variables).to.be.a('object');
+      expect(Object.keys(variables)).to.have.length(0);
     });
-})
-
-context('empty sass-file', function() {
-    describe('getVariables()', function() {
-      function testFn() {
-        const sass = '';
-        return parseVariables(getVariables(sass));
-      }
-
-      it('should not throw', function() {
-        expect(testFn).to.not.throw(TypeError);
-      });
-
-      it('should be an empty object', function(){
-        const variables = testFn();
-        expect(variables).to.be.a('object');
-        expect(Object.keys(variables)).to.have.length(0);
-      })
-    });
-})
+  });
+});
